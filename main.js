279a79777585f730f4afd645b0b06307e742afd5
LeeJun14 <777ljy777@gmail.com>
2023-04-18 09:35:09 +0900

    interactive web clone

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
 (() => {
+    const actions = {
+        birdFlies(key) {
+            if(key) {
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(
+                    ${window.innerWidth}px)`;
+            } else {
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100%)`;
+            }
+        },
+        birdFlies2(key) {
+            if(key) {
+                document.querySelector('[data-index="5"] .bird').style.transform = `translate(
+                    ${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`;
+            } else {
+                document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100%)`;
+            }
+        }
+    };
+
+
+
     const stepElems = document.querySelectorAll('.step');
     const graphicElems = document.querySelectorAll('.graphic-item');
     // 현재 활성화된(visible 클래스가 붙은) .graphic-item을 지정
@@ -15,12 +36,18 @@
         graphicElems[i].dataset.index = i;
     }
 
-    function activate() {
+    function activate(action) {
         currentItem.classList.add('visible');
+        if(action) {
+            actions[action](true);
+        }
     }
 
-    function inactivate() {
+    function inactivate(action) {
         currentItem.classList.remove('visible'); 
+        if(action) {
+            actions[action](false);
+        }
     }
 
     window.addEventListener('scroll', () => {
@@ -35,12 +62,15 @@
             if(boundingRect.top > window.innerHeight * 0.1 &&
                 boundingRect.top < window.innerHeight * 0.8) {
                 
-                    inactivate();
+                    inactivate(currentItem.dataset.action);
                     currentItem = graphicElems[step.dataset.index];
-                    activate();
+                    activate(currentItem.dataset.action);
             }
         }
-        console.log(temp);
+    });
+
+    window.addEventListener('load', () => {
+        setTimeout(() => scrollTo(0, 0), 100);
     });
 
     activate();
